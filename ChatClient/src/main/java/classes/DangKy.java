12e09c8a6285c80d515963aebcb18730d702f616
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-03-16 16:42:51 +0700

    thêm đăng nhập đăng ký và kết nối mysql

---
+++ ChatClient/src/main/java/classes/DangKy.java
@@ -0,0 +1,257 @@
+package classes;
+
+import classes.DataBase_Connect;
+import java.sql.*;
+
+public class DangKy extends javax.swing.JFrame {
+
+    DataBase_Connect cn = new DataBase_Connect();
+    Connection conn;
+
+    public DangKy() {
+        initComponents();
+        warning_name.setVisible(false);
+        warning_pass.setVisible(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        Dangky_label = new javax.swing.JLabel();
+        txtname = new javax.swing.JTextField();
+        txtpass = new javax.swing.JTextField();
+        passL = new javax.swing.JLabel();
+        nameL = new javax.swing.JLabel();
+        xemaccount = new javax.swing.JButton();
+        dangky_B = new javax.swing.JButton();
+        warning_name = new javax.swing.JLabel();
+        warning_pass = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(247, 247, 247));
+
+        Dangky_label.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        Dangky_label.setText("ĐĂNG KÝ");
+
+        passL.setText("Mật khẩu");
+
+        nameL.setText("Họ tên");
+
+        xemaccount.setText("Xem thông tin");
+        xemaccount.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                xemaccountActionPerformed(evt);
+            }
+        });
+
+        dangky_B.setText("Đăng ký");
+        dangky_B.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dangky_BActionPerformed(evt);
+            }
+        });
+
+        warning_name.setText("Username chỉ tối đa 8 kí tự");
+
+        warning_pass.setText("Mật khẩu phải có ít nhất 6 kí tự");
+
+        jButton1.setText("Tôi đã có tài khoản");
+        jButton1.setBorder(null);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(149, 149, 149)
+                        .addComponent(Dangky_label))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(passL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(nameL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addComponent(dangky_B)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+                                    .addComponent(xemaccount))
+                                .addComponent(txtname, javax.swing.GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE)
+                                .addComponent(txtpass))
+                            .addComponent(warning_name)
+                            .addComponent(warning_pass)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(141, 141, 141)
+                        .addComponent(jButton1)))
+                .addContainerGap(97, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(Dangky_label, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nameL))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(warning_name)
+                .addGap(2, 2, 2)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtpass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(passL))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(warning_pass)
+                .addGap(21, 21, 21)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(xemaccount)
+                    .addComponent(dangky_B))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1)
+                .addContainerGap(14, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void xemaccountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_xemaccountActionPerformed
+        try {
+            conn = cn.getJDbConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from account");
+            while (rs.next()) {
+                System.out.println("Name: " + rs.getString("username") + "\n" + "Pass: " + rs.getString("password"));
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_xemaccountActionPerformed
+
+    private void dangky_BActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dangky_BActionPerformed
+        String username = txtname.getText();
+        String password = txtpass.getText();
+
+        warning_name.setVisible(false);
+        warning_pass.setVisible(false);
+
+        if (username.length() == 0) {
+            warning_name.setText("Không được để trống username!!!");
+            warning_name.setVisible(true);
+            return;
+        }
+
+        if (username.length() > 8) {
+            warning_name.setText("Username chỉ tối đa 8 kí tự");
+            warning_name.setVisible(true);
+            return;
+        }
+
+        if (password.length() == 0) {
+            warning_pass.setText("Không được để trống password!!!");
+            warning_pass.setVisible(true);
+            return;
+        }
+
+        if (password.length() < 6) {
+            warning_pass.setText("Mật khẩu phải có ít nhất 6 kí tự");
+            warning_pass.setVisible(true);
+            return;
+        }
+
+        conn = cn.getJDbConnection();  //Trỏ tới phương thức trong DataBase_Connect
+
+        String sql = "INSERT INTO Account (username, password) VALUES (?, ?)";  //Chèn thông tin vào bảng account
+
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            int rowsAffected = pstmt.executeUpdate();
+
+            pstmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_dangky_BActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.setVisible(false);
+
+        // Hiển thị giao diện đăng nhập
+        new DangNhap().setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DangKy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DangKy().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Dangky_label;
+    private javax.swing.JButton dangky_B;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel nameL;
+    private javax.swing.JLabel passL;
+    private javax.swing.JTextField txtname;
+    private javax.swing.JTextField txtpass;
+    private javax.swing.JLabel warning_name;
+    private javax.swing.JLabel warning_pass;
+    private javax.swing.JButton xemaccount;
+    // End of variables declaration//GEN-END:variables
+}
